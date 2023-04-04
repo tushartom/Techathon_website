a02f77c2dc52322552ab9c1f751a43e2e2a0caff
Tushar <tushartom735@gmail.com>
2023-04-04 13:17:27 +0530

    edit nav links

--- public/js/main.js
+++ public/js/main.js
@@ -49,3 +49,12 @@ questions.forEach(question => {
     questionContent.classList.toggle("hidden")
   })
 })
+
+document.querySelector("#nav-menu").addEventListener("click", function(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    // console.log(e.target);
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: "smooth"});
+  }
+});
