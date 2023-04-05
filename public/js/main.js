88042916aa2e0a50b2ab4d3ab2b3998ed00ab32d
Tushar <tushartom735@gmail.com>
2023-04-05 13:03:37 +0530

    added section animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,42 @@
-const toggleBtn = document.querySelector("#toggle-btn")
-navMenu = document.querySelector("#nav-menu")
+const toggleBtn = document.querySelector("#toggle-btn");
+const navMenu = document.querySelector("#nav-menu");
+const tabsContainer = document.querySelector(".process__tabs-container");
+const tabs = document.querySelectorAll(".process__tab");
+const tabsContent = document.querySelectorAll(".process__content");
+const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
 
-toggleBtn.addEventListener("click", showMenu)
-// Show menu
 function showMenu() {
   navMenu.classList.toggle("h-0")
   navMenu.classList.toggle("opacity-0")
 }
-// Hide menu when click on link
-const navLinks = document.querySelectorAll("#nav-menu a");
-navLinks.forEach(navLink => {
-  navLink.addEventListener("click", ()=> {
-    // console.log("clicked")
-    navMenu.classList.add("h-0")
-    navMenu.classList.add("opacity-0")
-  })
+
+// toggle btn for mobile menu
+toggleBtn.addEventListener("click", showMenu)
+
+// Navigation Links
+navMenu.addEventListener("click", function(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: "smooth"});
+    // Hide menu after click
+    navMenu.classList.add("h-0");
+    navMenu.classList.add("opacity-0");
+  }
+});
+
+// tabs functionality
+tabsContainer.addEventListener("click", function(e) {
+  const clicked = e.target.closest(".process__tab");
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove("active"));
+  tabsContent.forEach(c => c.classList.remove("process__content-active"));
+
+  clicked.classList.add("active");
+  document.querySelector(`.process__content--${clicked.dataset.tab}`).classList.add("process__content-active");
 })
-
-const tabButtons = document.querySelectorAll(".tab-btn")
-const tabContents = document.querySelectorAll(".tab-content")
-// console.log(tabButtons[0])
-tabButtons.forEach((tabBtn) => {
-  tabBtn.addEventListener("click", () => {
-    const tabContainer = document.querySelector(".tab-container")
-    const tabNumber = tabBtn.dataset.forTab
-    const tabContent = tabContainer.querySelector(`.tab-content[data-tab="${tabNumber}"`)
-    
-    for (i = 0; i < tabContents.length; i++) {
-      tabContents[i].style.display = "none";
-    }
-    for (i = 0; i < tabButtons.length; i++) {
-      tabButtons[i].className = tabButtons[i].className.replace(" active", "");
-    }
-    tabContent.style.display = "block";
-    tabBtn.className += " active";
-  })
-})
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
 
 const questions = document.querySelectorAll(".single-faq")
 questions.forEach(question => {
@@ -50,11 +47,41 @@ questions.forEach(question => {
   })
 })
 
-document.querySelector("#nav-menu").addEventListener("click", function(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("nav-link")) {
-    // console.log(e.target);
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({behavior: "smooth"});
-  }
-});
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+})
+
+headerObserver.observe(header);
+
+// reveal sections 
+
+const sections = document.querySelectorAll(".section");
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section__hidden");
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+})
+
+sections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add("section__hidden");
+})
